3e9c4a2e745cd1e3a314d3a1a3874b80cfd33469
Susanna Maria <susanna@olsoni.de>
2020-05-17 13:22:52 +0200

    Prepare testrun

--- train.py
+++ train.py
@@ -3,11 +3,11 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 from agent import MADDPG
-from utils import draw
 import argparse
 from tqdm import tqdm
 
 unity_environment_path = "./Tennis_Linux/Tennis.x86_64"
+
 
 def draw(scores, path="fig.png", title="Performance", xlabel="Episode #", ylabel="Score"):
     fig = plt.figure()
@@ -18,12 +18,13 @@ def draw(scores, path="fig.png", title="Performance", xlabel="Episode #", ylabel
     plt.xlabel(xlabel)
     plt.savefig(path)
 
+
 def train(args):
     # prepare environment
     brain_name = env.brain_names[0]
     brain = env.brains[brain_name]
     env_info = env.reset(train_mode=True)[brain_name]
-    
+
     num_agents = len(env_info.agents)
     print('Number of agents:', num_agents)
 
@@ -31,24 +32,25 @@ def train(args):
     action_size = brain.vector_action_space_size
     print('Size of each action:', action_size)
 
-    # examine the state space 
+    # examine the state space
     states = env_info.vector_observations
     state_size = states.shape[1]
-    print('There are {} agents. Each observes a state with length: {}'.format(states.shape[0], state_size))
+    print('There are {} agents. Each observes a state with length: {}'.format(
+        states.shape[0], state_size))
     print('The state for the first agent looks like:', states[0])
 
-    agent = MADDPG(state_size, 
+    agent = MADDPG(state_size,
                    action_size,
-                   lr_actor = args.lr_actor,
-                   lr_critic = args.lr_critic,
-                   lr_decay = args.lr_decay,
-                   replay_buff_size = args.replay_buff_size,
-                   gamma = args.gamma,
-                   batch_size = args.batch_size,
-                   random_seed = args.random_seed,
-                   soft_update_tau = args.soft_update_tau
-                 )
-    
+                   lr_actor=args.lr_actor,
+                   lr_critic=args.lr_critic,
+                   lr_decay=args.lr_decay,
+                   replay_buff_size=args.replay_buff_size,
+                   gamma=args.gamma,
+                   batch_size=args.batch_size,
+                   random_seed=args.random_seed,
+                   soft_update_tau=args.soft_update_tau
+                   )
+
     total_rewards = []
     avg_scores = []
     max_avg_score = -1
@@ -57,48 +59,59 @@ def train(args):
     noise_t = args.epsilon
     noise_decay = args.epsilon_decay
     latest_avg_score = -1
-    worsen_tolerance = threshold_init  # for early-stopping training if consistently worsen for # episodes
-    for i_episode in  range(1, 1+args.num_episodes):
-       
-        env_inst = env.reset(train_mode=True)[brain_name]    # reset the environment
+    # for early-stopping training if consistently worsen for # episodes
+    worsen_tolerance = threshold_init
+    for i_episode in range(1, 1+args.num_episodes):
+
+        env_inst = env.reset(train_mode=True)[
+            brain_name]    # reset the environment
         states = env_inst.vector_observations                # get the current state
-        scores = np.zeros(num_agents)                        # initialize score array
+        # initialize score array
+        scores = np.zeros(num_agents)
         dones = [False]*num_agents
         while not np.any(dones):
-            actions = agent.act(states,noise_t)              # select an action
-            env_inst = env.step(actions)[brain_name]         # send the action to the environment
+            # select an action
+            actions = agent.act(states, noise_t)
+            # send the action to the environment
+            env_inst = env.step(actions)[brain_name]
             next_states = env_inst.vector_observations       # get the next state
             rewards = env_inst.rewards                       # get the reward
             dones = env_inst.local_done                      # see if episode has finished
             agent.update(states, actions, rewards, next_states, dones)
-            
+
             noise_t *= noise_decay
             scores += rewards                                # update scores
-            states = next_states 
-        
+            states = next_states
+
         episode_score = np.max(scores)
         total_rewards.append(episode_score)
-        print("\rEpisodic {} Score: {:.4f}\t Avg Score: {:.4f}".format(i_episode, episode_score,latest_avg_score),end=' ')
-        
-        if max_score <= episode_score:                     
+        print("\rEpisodic {} Score: {:.4f}\t Avg Score: {:.4f}".format(
+            i_episode, episode_score, latest_avg_score), end=' ')
+
+        if max_score <= episode_score:
             max_score = episode_score
-            agent.save(args.model_path)                     # save best model so far
-        
-        if len(total_rewards) >= args.test_n_run:                       # record avg score for the latest 100 steps
-            latest_avg_score = sum(total_rewards[(len(total_rewards)-args.test_n_run):]) / args.test_n_run
+            # save best model so far
+            agent.save(args.model_path)
+
+        # record avg score for the latest 100 steps
+        if len(total_rewards) >= args.test_n_run:
+            latest_avg_score = sum(
+                total_rewards[(len(total_rewards)-args.test_n_run):]) / args.test_n_run
             avg_scores.append(latest_avg_score)
-          
+
             if max_avg_score <= latest_avg_score:           # record better results
                 worsen_tolerance = threshold_init           # re-count tolerance
                 max_avg_score = latest_avg_score
-            else:                                           
-                if max_avg_score > 0.5:                     
+            else:
+                if max_avg_score > 0.5:
                     worsen_tolerance -= 1                   # count worsening counts
                     print("Loaded from last best model.")
-                    agent.load(args.model_path)             # continue from last best-model
+                    # continue from last best-model
+                    agent.load(args.model_path)
                 if worsen_tolerance <= 0:                   # earliy stop training
                     print("Early Stop Training.")
                     break
+
 
 def test(args):
     brain_name = env.brain_names[0]
@@ -111,35 +124,39 @@ def test(args):
     # dim of each action
     action_size = brain.vector_action_space_size
     print('Size of each action:', action_size)
-    
-    # dim of the state space 
+
+    # dim of the state space
     states = env_info.vector_observations
     state_size = states.shape[1]
-    
+
     agent = MADDPG(state_size, action_size)
-                    
+
     agent.load(args.model_path)
 
     test_scores = []
     for i_episode in tqdm(range(1, 1+args.test_n_run)):
-        scores = np.zeros(num_agents)                       # initialize the scores
-        env_info = env.reset(train_mode=True)[brain_name]   # reset the environment
+        # initialize the scores
+        scores = np.zeros(num_agents)
+        env_info = env.reset(train_mode=True)[
+            brain_name]   # reset the environment
         states = env_info.vector_observations               # get the current states
         dones = [False]*num_agents
         while not np.any(dones):
             actions = agent.act(states)                     # select actions
-            env_info = env.step(actions)[brain_name]        # send the actions to the environment
+            # send the actions to the environment
+            env_info = env.step(actions)[brain_name]
             next_states = env_info.vector_observations      # get the next states
             rewards = env_info.rewards                      # get the rewards
             dones = env_info.local_done                     # see if episode has finished
             scores += rewards                               # update the scores
-            states = next_states                            # roll over the states to next time step
+            # roll over the states to next time step
+            states = next_states
 
         test_scores.append(np.max(scores))
-   
+
     avg_score = sum(test_scores)/len(test_scores)
     print("Test Score: {}".format(avg_score))
-   
+
     return avg_score
 
 
@@ -155,15 +172,15 @@ if __name__ == "__main__":
     parser.add_argument('--batch_size', default=int(64), type=int)
     parser.add_argument('--random_seed', default=int(999), type=int)
     parser.add_argument('--soft_update_tau', default=float(1e-3), type=float)
-    parser.add_argument('--model_path',default='best_model.checkpoint')
-    parser.add_argument('--test_n_run',default=int(100), type=int)   
+    parser.add_argument('--model_path', default='best_model.checkpoint')
+    parser.add_argument('--test_n_run', default=int(100), type=int)
     parser.add_argument('--epsilon', default=float(1.0), type=float)
     parser.add_argument('--epsilon_decay', default=float(.995), type=float)
     parser.add_argument('--main_n_loop', default=int(5), type=int)
 
     env = UnityEnvironment(file_name=unity_environment_path)
     args = parser.parse_args()
-    scores=[]
+    scores = []
     mp = args.model_path
     for i in range(args.main_n_loop):
         args.model_path = "{:02d}_".format(i)+mp
@@ -175,4 +192,3 @@ if __name__ == "__main__":
     print(np.min(scores))
     print(np.max(scores))
     print(np.mean(scores))
-                    
